715b9b6d8adc696d9128e4925a12199ba641ede4
Gábriel Németh <gabriel.nemeth@deinebaustoffe.de>
2022-06-24 13:37:30 +0200

    Refactor everything into proper classes

--- chain.py
+++ chain.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-from typing import List, Optional
+from typing import List
 import sys
 
 
@@ -25,71 +25,89 @@ class EscapedOrChainer(ChainerInterface):
         return '\\|'.join(chain)
 
 
-def get_arg_filename() -> Optional[str]:
-    filename = sys.argv[1:2]
-    if not filename:
-        return None
+class ArgsReader:
+    def get_arg_filename(self) -> str:
+        filename = sys.argv[1:2]
+        if not filename:
+            return ''
 
-    return filename.pop()
+        return filename.pop()
 
+    def get_arg_title(self) -> str:
+        title = sys.argv[2:3]
+        if not title:
+            return '[NO TITLE]'
 
-def get_arg_title() -> str:
-    title = sys.argv[2:3]
-    if not title:
-        return '[NO TITLE]'
-
-    return title.pop()
-
-
-def read_data(filename: Path) -> list:
-    with filename.open('r') as file:
-        data = []
-        lines = file.readlines()
-
-        for line in lines:
-            line = line.strip(chr(10))
-            line = line.strip(chr(13))
-            data.append(line)
-
-    return data
+        return title.pop()
 
 
-def get_chainers() -> List['ChainerInterface']:
-    return [
-        PlainChainer(),
-        QuotedChainer(),
-        EscapedOrChainer(),
-    ]
+class FileReader:
+    def read_data(self, filename: str) -> list:
+        filename = self._build_path(filename)
+
+        with filename.open('r') as file:
+            data = []
+            lines = file.readlines()
+
+            for line in lines:
+                line = line.strip(chr(10))
+                line = line.strip(chr(13))
+                data.append(line)
+
+        return data
+
+    def _build_path(self, filename: str) -> 'Path':
+        if not filename:
+            print('No input file provided. Please enter a filename as a parameter. e.g.: chain source.txt')
+            quit()
+
+        filename = Path(filename)
+
+        if not filename.is_file():
+            print(
+                f"The specified file ('{filename}') does not exist or the path is incorrect. Please check your input and try again.")
+            quit()
+
+        return filename
 
 
-def remove_duplicates(list_to_filter: list) -> list:
-    return list(set(list_to_filter))
+class App:
+    def __init__(self):
+        self._args_reader = ArgsReader()
+        self._file_reader = FileReader()
 
+    def run(self):
+        filename = self._args_reader.get_arg_filename()
+        data = self._file_reader.read_data(filename)
+        data = self._remove_duplicates(data)
 
-def display_block(block: str, title: str) -> None:
-    print(title)
-    print('-' * len(title))
-    print(block)
-    print('')
+        title = self._args_reader.get_arg_title()
+        chainers = self._get_chainers()
+        for chainer in chainers:
+            block = chainer.chain(data)
+            self._display_block(block, title)
+
+        print(f"{len(data)} elements chained.")
+
+    def _get_chainers(self) -> List['ChainerInterface']:
+        return [
+            PlainChainer(),
+            QuotedChainer(),
+            EscapedOrChainer(),
+        ]
+
+    def _remove_duplicates(self, list_to_filter: list) -> list:
+        return list(set(list_to_filter))
+
+    def _display_block(self, block: str, title: str) -> None:
+        display_format = f"""{title}
+{'-' * len(title)}
+{block}
+"""
+
+        print(display_format)
 
 
 if __name__ == '__main__':
-    filename = get_arg_filename()
-    if not filename:
-        print('No input file provided. Please enter a filename as a parameter. e.g.: chain source.txt')
-        quit()
-
-    filename = Path(filename)
-    if not filename.is_file():
-        print(f"The specified file ('{filename}') does not exist or the path is incorrect. Please check your input and try again.")
-        quit()
-
-    title = get_arg_title()
-    data = remove_duplicates(read_data(filename))
-    chainers = get_chainers()
-
-    for chainer in chainers:
-        block = chainer.chain(data)
-        display_block(block, title)
-
-    print(f"{len(data)} elements chained.")
+    app = App()
+    app.run()
